e65916a8ed9359039f36278a5012820c24afbdf0
m3ayush <ayushbhat0518@gmail.com>
2025-07-16 18:22:58 +0530

    user component

--- src/components/User/User.jsx
+++ src/components/User/User.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
+import { useParams } from 'react-router-dom'
 
 function User() {
+        const {userid}= useParams()
   return (
     <div>
-      User:
+      User:{userid}
     </div>
   )
 }
